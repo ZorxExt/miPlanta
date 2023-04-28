607860538a87ef8b83d61718cb6c6787ca91d2ac
ZorxExt <franciscosabatecastillo@gmail.com>
2023-04-27 22:53:20 -0300

    cosas

---
+++ JS/scripts.js
@@ -0,0 +1,12 @@
+function start() {
+    document.getElementById("startNEW-container").style.display = "none";
+    document.getElementById("startTXT-container").style.display = "block"
+}
+
+
+
+function info() {
+    document.getElementById("startTXT-container").style.display = "none";
+    document.getElementById("aboutCODE-container").style.display = "block"
+}
+
