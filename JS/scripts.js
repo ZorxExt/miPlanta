fae60dee6ac91fb311c61e411ac7868188a3bcb8
ZorxExt <franciscosabatecastillo@gmail.com>
2024-05-07 13:04:49 -0300

    Ultima version sin framework

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,66 +1,17 @@
 function start() {
-  document.getElementById("startNEW-container").style.display = "none";
-  document.getElementById("startTXT-container").style.display = "block";
+  document.getElementsByClassName("start_1")[0].style.transform =
+    "translateX(-1000px)";
+  setTimeout(function () {
+    document.getElementsByClassName("codigo-vinculacion")[0].style.transform =
+      "translateX(0)";
+  }, 1000);
 }
 
 function info() {
-  document.getElementById("startTXT-container").style.display = "none";
-  document.getElementById("aboutCODE-container").style.display = "block";
+  document.getElementsByClassName("codigo-vinculacion")[0].style.transform =
+    "translateX(-1500px)";
+  setTimeout(function () {
+    document.getElementsByClassName("codigo-vinculacion-about")[0].style.transform =
+      "translateX(0)";
+  }, 1000);
 }
-
-function closeFinalizado() {
-  document.getElementById("finalizado").style.opacity = "0";
-  document.getElementById("finalizado").style.pointerEvents = "none";
-}
-function cerrarWarning(){
-  document.getElementsByClassName("warning")[0].style.opacity = "0"
-  document.getElementsByClassName("warning")[0].style.pointerEvents = "none"
-  
-}
-
-function FileOnChange(p, previewElem, dataElem) {
-  var file = p.files[0];
-  imgSrc = URL.createObjectURL(file);
-  previewElem.src = imgSrc;
-
-  let imgRead = new FileReader();
-  imgRead.onload = function (e) {
-    var allData = e.target.result;
-    var imgData = allData.split("base64,")[1];
-    dataElem.value = imgData;
-  };
-
-
-  imgRead.readAsDataURL(file);
-}
-
-const form = document.getElementById("contact-form");
-
-const cargando = document.getElementById("container-carga");
-const finalizado = document.getElementById("finalizado");
-
-const register = document.getElementById("register")
-
-
-form.addEventListener("submit", function (e) {
-
-  e.preventDefault();
-  const data = new URLSearchParams(new FormData(form));
-  const action = e.target.action;
-  cargando.style.display = "grid";
-  register.style.backgroundColor = "white";
-  form.style.pointerEvents = "none";
-  document.getElementById("register-text").textContent = "";
-  fetch(action, {
-    mode: "no-cors",
-    method: "POST",
-    body: data
-  }).then(() => {
-    finalizado.style.opacity = 1;
-    finalizado.style.pointerEvents = "auto";
-    form.style.pointerEvents = "auto";
-    cargando.style.display = "none";
-    register.style.backgroundColor = "rgb(6, 190, 89)"
-    document.getElementById("register-text").textContent = "Enviar";
-  });
-});
